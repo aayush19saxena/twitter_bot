82093ce0ee50c69ae38231b6125d0986705a6787
Aayush <aayush19saxena@gmail.com>
2016-04-18 21:27:06 -0700

    Initial Commit

---
+++ db/seeds.rb
@@ -0,0 +1,18 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Response.create(message: "Meditation can help with #insomnia! http://nyti.ms/1LCs8hF Here's an app I love: calm.com")
+Response.create(message: "Meditation can help with #insomnia! http://nyti.ms/1LCs8hF Here's an app I love: headspace.com")
+Response.create(message: "Meditation can help with #insomnia! http://nyti.ms/1LCs8hF Here's an app I love: saagara.com")
+Response.create(message: "Meditation can help with #insomnia! http://nyti.ms/1LCs8hF Here's an app I love: omvana.com")
+Response.create(message: "Meditation can help with #insomnia! http://nyti.ms/1LCs8hF Here's an app I love: buddhify.com")
+Response.create(message: "Test SRK #badebadesheher!")
+Response.create(message: "Test Gaandu #kritkatvij!")
+Response.create(message: "This is a great tweet! :)") 
+Response.create(message: "This is a mediocore tweet : | ")
+Response.create(message: "This tweet is downright disgraceful :( ")
